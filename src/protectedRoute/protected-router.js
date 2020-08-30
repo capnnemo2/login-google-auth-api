f260abc5330c85af5b4fc3bd941bfa23109a84f9
Ben Hernandez <benjammin234@gmail.com>
2020-08-30 16:42:00 -0700

    Added google auth library

--- src/protectedRoute/protected-router.js
+++ src/protectedRoute/protected-router.js
@@ -1,12 +1,30 @@
 const express = require("express");
-const ProtectedService = requier("./protected-service");
+
+const { OAuth2Client } = require("google-auth-library");
+const { token } = require("morgan");
+
+const ProtectedService = require("./protected-service");
 
 const protectedRouter = express.Router();
 
-protectedRouter.route("/").get(requireAuth, (req, res, next) => {
+const client = new OAuth2Client(process.env.CLIENT_ID);
+
+async function verify() {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID,
+  });
+  const payload = ticket.getPayload();
+  const userid = payload["sub"];
+}
+verify().catch(console.error);
+
+protectedRouter.route("/").get((req, res, next) => {
   ProtectedService.getAll(req.app.get("db"))
     .then((all) => {
       res.json();
     })
     .catch(next);
 });
+
+module.exports = protectedRouter;
